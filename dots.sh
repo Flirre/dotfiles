83f37d300bc7ff410268cecf2c7d497e76e0aa3d
Filip Lindahl <gysztaf@gmail.com>
2018-12-30 22:11:03 +0100

    checks if i3 is used before backing up

--- dots.sh
+++ dots.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-(cp -r /home/$USER/.config/i3/ ./ && echo "Successfully backed up i3 config.") || echo "i3 backup failed."
+if [ "${XDG_CURRENT_DESKTOP}" == "i3" ]; then
+       (cp -r /home/$USER/.config/i3/ ./ && echo "Successfully backed up i3 config.") || echo "i3 backup failed."
+fi
 (cp /home/$USER/.config/compton.conf ./ && echo "Successfully backed up Compton config.") || echo "Compton backup failed."
 (cp -r /home/$USER/.config/polybar/ ./ && echo "Successfully backed up Polybar config.") || echo "Polybar backup failed."
 (cp -r /home/$USER/.config/kitty/ ./ && echo "Successfully backed up KiTTy config.") || echo "KiTTy backup failed."
