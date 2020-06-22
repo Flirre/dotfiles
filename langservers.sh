6dd5f80da72dec1784960ac62e8db8a821af3c78
Filip Lindahl <filip.g.lindahl@gmail.com>
2020-06-22 18:18:57 +0200

    list of langservers

--- langservers.sh
+++ langservers.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+#langservers for:
+#CSS
+#bash
+#HTML
+#JSON
+#Vue
+#YAML
+#typescript
+
 npm install -g vscode-css-languageserver-bin bash-language-server vscode-html-languageserver-bin vscode-json-languageserver vls yaml-language-server typescript-language-server typescript
