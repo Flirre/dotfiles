57353d777f7ae3104f3b00d2182a9a8c202bef4a
Filip Lindahl <filip.lindahl@skf.com>
2021-08-23 15:17:33 +0200

    yay -> paru

--- fish/functions/update.fish
+++ fish/functions/update.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
 
 function update
-    sudo pacman -Syu; and yay
+    sudo pacman -Syu; and paru
 end
--- setup_computer.fish
+++ setup_computer.fish
@@ -14,20 +14,20 @@ function setup_computer
     if test (count $argv) -eq 1
         set -l arg $argv[1]
         switch "$arg"
-            case "-a" "--all"
+            case -a --all
                 sudo pacman -S - <~/dotfiles/packages/base
-                yay -S - <~/dotfiles/packages/base
+                paru -S - <~/dotfiles/packages/base
                 sudo pacman -S - <~/dotfiles/packages/home
-                yay -S - <~/dotfiles/packages/home
+                paru -S - <~/dotfiles/packages/home
                 sudo pacman -S - <~/dotfiles/packages/work
-                yay -S - <~/dotfiles/packages/work
-            case "-b" "--base"
+                paru -S - <~/dotfiles/packages/work
+            case -b --base
                 sudo pacman -S - <~/dotfiles/packages/base
-                yay -S - <~/dotfiles/packages/base-aur
-            case "-h" "--home"
-                yay -S - <~/dotfiles/packages/home
-            case "-w" "--work"
-                yay -S - <~/dotfiles/packages/work
+                paru -S - <~/dotfiles/packages/base-aur
+            case -h --home
+                paru -S - <~/dotfiles/packages/home
+            case -w --work
+                paru -S - <~/dotfiles/packages/work
             case '*'
                 permitted_flags
         end
